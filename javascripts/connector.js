8ed67d83adcd16b06d0fb64b27081755cde60e67
Adam Goldstein <agoldstein@mitre.org>
2012-01-30 15:01:19 -0500

    Content height is now properly relative

--- javascripts/connector.js
+++ javascripts/connector.js
@@ -8,8 +8,16 @@ $(document).ready(function() {
     if (!alreadyShown)
       content.reveal();
   });
+  
+  // A bit of a hack - On load, set the height of content to be relative to the whole window
+  $(".content").height($(window).height() / 1.85);
+  $(window).resize(function() {
+    // Also keep the height relative if the window is ever resized
+    $(".content").height($(window).height() / 1.85);
+  })
 });
 
+// Let's make slightly prettier shows and hides
 jQuery.fn.extend({
   reveal: function() {
     return this.each(function() {    
